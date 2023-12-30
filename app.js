d6053378738980264a92bd548d3bf81ec221ebde
jadepegasus <jadepegasus235@gmail.com>
2023-12-30 20:43:59 +0700

    second

--- app.js
+++ app.js
@@ -29,7 +29,12 @@ const app = express();
 
 // link mac dinh
 app.use(express.static("public"));
-
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', 'https://frontend-social-r1ky.onrender.com');
+  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+  res.header('Access-Control-Allow-Headers', 'Content-Type');
+  next();
+});
 app.use(
   cors({
     methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
