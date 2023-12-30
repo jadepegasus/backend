06a3105db5ca6fed47794d65c78609c24d0d0072
jadepegasus <jadepegasus235@gmail.com>
2023-12-30 22:09:52 +0700

    muoi

--- app.js
+++ app.js
@@ -13,14 +13,14 @@ const mongoose = require("mongoose");
 require("dotenv").config();
 
 // Đọc chứng chỉ và private key
-const credentials = {
-  key: fs.readFileSync("./key/key1.pem", "utf8"),
-  cert: fs.readFileSync("./key/www_habuiphuc_id_vn.chained.crt", "utf8"),
-};
+// const credentials = {
+//   key: fs.readFileSync("./key/key1.pem", "utf8"),
+//   cert: fs.readFileSync("./key/www_habuiphuc_id_vn.chained.crt", "utf8"),
+// };
 
 // connect to mongodb
 mongoose
-  .connect(process.env.MONGODB_URI || "mongodb://localhost:27017/mangxahoi")
+  .connect(process.env.MONGODB_URI)
   .catch((err) => {
     console.log(err);
   });
@@ -58,11 +58,53 @@ app.use("/", Router);
 app.use((req, res) => {
   res.sendFile(path.join(__dirname, 'public', 'index.html'))
 })
-app.listen(process.env.PORT || 8080, () => {
-  console.log(`Server is running on port ${process.env.PORT || 8080}`);
-});
+// app.listen(process.env.PORT || 8080, () => {
+//   console.log(`Server is running on port ${process.env.PORT || 8080}`);
+// });
 
 // Tạo máy chủ HTTPS va socket.io
+const httpsServer = https.createServer(credentials, app);
+const io = new Server(httpsServer, {
+  cors: {
+    origin: true,
+    credentials: true
+  },
+});
+io.engine.use(sessionMiddleware);
+io.on("connection", (socket) => {
+  // console.log('new user')
+  let user_id = null;
+  socket.on("active", (_id) => {
+    user_id = _id;
+    if (user_id) {
+      UserService.setActived(user_id);
+      socket.join(user_id);
+    }
+  });
+  socket.on("notify", (data) => {
+    socket.to(data?.user_two_id).emit("notify", data.message);
+  });
+  socket.on('entering', ({ sender, receiver }) => {
+    socket.to(receiver).emit('entering', { sender, receiver })
+  })
+  socket.on('onmessage', (message) => {
+    socket.to(message?.receiver).emit('onmessage', message)
+  })
+  socket.on('newmessage', ({ receiver, message }) => {
+    socket.to(receiver).emit('newmessage', message)
+  })
+  socket.on("disconnect", () => {
+    setTimeout(() => {
+      if (user_id && !io.sockets.adapter.rooms.get(user_id)) {
+        UserService.unActived(user_id);
+      }
+    }, 5000);
+  });
+});
 
+httpsServer.listen(process.env.PORT || 8080, () => {
+  console.log(httpsServer.address())
+  console.log(`Server running on port ${process.env.PORT || 8080}`);
+});
 
 module.exports = app;
